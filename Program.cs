7944676d054663b39cb887ca633fe7c23d4401ae
Tillsxp <112383033+Tillsxp@users.noreply.github.com>
2022-11-24 07:18:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace övning;
+class Program
+{
+    static void Main()
+    {   
+
+        var mobile = new Mobile ();
+        var mobile1 = new Mobile ();
+
+        Console.WriteLine(@"Mobile Information:
+        UserId: {0}
+        FullName: {1}
+        MobileNumber: {2}", mobile.UserId, mobile.FullName, mobile.MobileNumber);
+    }
+}
